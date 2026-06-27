c69be9675fb080d813d5f1d1c22a448d7a993d37
chrollolucilfer-v <guptabhaskar9897@gmail.com>
2026-06-27 14:38:08 +0530

    bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+
+
+import { useState } from "react"
+
 export default function App() {
+  const [color,setColor]  = useState("olive")
+
+  
+
   return (
-    <div className="">
-      <h1 className="text-5xl font-bold text-blue-600">
-        Tailwind is Working! 🎉
-      </h1>
+    
+    <div className= "w-full h-screen duration-200" 
+    style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12 px-2 inset-x-0">
+        
+      <div className="flex flex-wrap gap-3 justify-center
+      shadow-lg bg-white px-3 py-2 rounded-3xl
+      ">
+        <button onClick={()=> setColor("red")} className = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}> Red </button>
+        <button onClick={()=> setColor("green")} className = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}}> Green </button>
+        <button onClick={()=> setColor("blue")}  className = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}> Blue </button>
+        <button onClick={()=> setColor("olive")} className = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "olive"}}> Olive </button>
+        <button onClick={()=> setColor("black")} className = "outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "black"}}> Black </button>
+      </div>
+      </div>
+
+
+
+
+
+
     </div>
   );
 }
