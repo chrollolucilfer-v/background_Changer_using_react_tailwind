5a16f8d17136446b4667f199c0a4767ba74e89d4
chrollolucilfer-v <guptabhaskar9897@gmail.com>
2026-06-27 13:27:55 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind is Working! 🎉
+      </h1>
+    </div>
+  );
+}
